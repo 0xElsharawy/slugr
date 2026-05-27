9a7ff433ffe7c32b931d7ef572597758a2735cb9
Ahmed <ahmedelsharawy117@gmail.com>
2026-05-27 03:30:43 +0300

    refactor(api): remove redis password

--- api/database/redis.py
+++ api/database/redis.py
@@ -2,14 +2,12 @@ import redis
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
-REDIS_PASSWORD = "slugr_redis_passwrod"
 
 
 def get_redis():
     client = redis.Redis(
         host=REDIS_HOST,
         port=REDIS_PORT,
-        password=REDIS_PASSWORD,
         decode_responses=True,
     )
     try:

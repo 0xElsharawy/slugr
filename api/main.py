f5ef95d6cf733ae029d26286b8d6fe9a23cb425a
Ahmed <ahmedelsharawy117@gmail.com>
2026-05-27 03:31:06 +0300

    refactor(api): cache data using redis

--- api/main.py
+++ api/main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, AnyHttpUrl
 
 from middlewares import logging
 from database.db import get_db
+from database.redis import get_redis
 from database.models import Url
 from utils import create_unique_slug, parse_expires_at
 
@@ -33,7 +34,9 @@ class CreateUrlRequest(BaseModel):
 
 
 @app.post("/urls")
-def create_url(payload: CreateUrlRequest, db: Session = Depends(get_db)):
+def create_url(
+    payload: CreateUrlRequest, db: Session = Depends(get_db), redis=Depends(get_redis)
+):
     stmt = select(Url).where(Url.url == str(payload.url))
     url = db.execute(stmt).scalar_one_or_none()
     if url:
@@ -43,14 +46,20 @@ def create_url(payload: CreateUrlRequest, db: Session = Depends(get_db)):
     url = Url(url=str(payload.url), short_code=slug, expires_at=expires_at)
     db.add(url)
     db.commit()
+    redis.set(slug, str(payload.url), ex=300)
     return {"short_code": slug}
 
 
 @app.get("/{short_code}")
-def redirect(short_code: str, db: Session = Depends(get_db)):
+def redirect(short_code: str, db: Session = Depends(get_db), redis=Depends(get_redis)):
+    url = redis.get(short_code)
+    if url:
+        print("URL from Redis:", url)
+        return RedirectResponse(url)
     stmt = select(Url).where(Url.short_code == short_code)
     url = db.execute(stmt).scalar_one_or_none()
     print("URL:", url.url if url else "None")
     if not url:
         raise HTTPException(status_code=404, detail="URL not found")
+    redis.set(short_code, url.url, ex=300)
     return RedirectResponse(url.url)
